d45847fc361580db302156aae8edfec1c26c0100
CaroBoschini <mboschini@frba.utn.edu.ar>
2024-10-17 18:56:53 -0300

    archivo secrets con docker

---
+++ secret.sh
@@ -0,0 +1,5 @@
+kubectl create secret docker-registry regcred \ 
+    --docker-server=https://index.docker.io/v1 \ 
+    --docker-username=micaelaa45 \
+    --docker-password='micaela24' \ 
+    --docker-email=mbmendoza20@gmail.com 
