9c997577950125300cc104d9733d1911e2666745
CaroBoschini <mboschini@frba.utn.edu.ar>
2024-10-19 20:27:07 -0300

    perdon mica

--- produccion/secret.sh
+++ produccion/secret.sh
@@ -1,5 +1,5 @@
 kubectl create secret docker-registry regcred \
   --docker-server=https://index.docker.io/v1/ \
-  --docker-username= \
-  --docker-password='' \
-  --docker-email=
+  --docker-username=micaelaa45 \
+  --docker-password='micaela24' \
+  --docker-email=mbmendoza@gmail.com
