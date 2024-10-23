a9b26dad8dae51171a4d0499ad28bc4274a5c496
Melina Bugallo <melinabugallo@gmail.com>
2024-10-22 23:07:14 -0300

    Update secret.sh

--- produccion/secret.sh
+++ produccion/secret.sh
@@ -1,5 +1,1 @@
-kubectl create secret docker-registry regcred \
-  --docker-server=https://index.docker.io/v1/ \
-  --docker-username=micaelaa45 \
-  --docker-password='micaela24' \
-  --docker-email=mbmendoza@gmail.com
+
